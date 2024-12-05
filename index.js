9a2d28324ab653eb2daeb645ce57c04d19c796da
mrhonneynive <38505964+mrhonneynive@users.noreply.github.com>
2024-12-06 00:11:42 +0300

    Solve menu problems for monday ogle and lunch

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const iconv = require("iconv-lite");
+const cheerio = require("cheerio");
 
 const app = express();
 const PORT = 3000;
@@ -9,7 +10,7 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Bileats API!");
 });
 
-app.get("/menu", async (req, res) => {
+app.get("/menu/:day", async (req, res) => {
   try {
     const response = await axios.get(
       "http://kafemud.bilkent.edu.tr/monu_eng.html",
@@ -19,10 +20,38 @@ app.get("/menu", async (req, res) => {
     );
     const html = iconv.decode(Buffer.from(response.data), "windows-1254");
 
-    res.set("Content-Type", "text/html; charset=utf-8");
-    res.send(html);
+    const $ = cheerio.load(html);
+
+    const menu = {
+      ogle: [],
+      lunch: [],
+      aksam: [],
+      dinner: [],
+      secmeli: [],
+      alternative: [],
+    };
+
+    for (let i = 4; i <= 8; i++) {
+      // 85 olması lazım
+      for (let j = 1; j <= 2; j++) {
+        const row = $(
+          `.MsoNormalTable tr:nth-of-type(2) .MsoNormalTable tr:nth-of-type(${i}) td:nth-of-type(${j}) p span`
+        )
+          .text()
+          .replace(/\s+/g, " ")
+          .trim();
+
+        if (j === 1) {
+          menu.ogle.push(row);
+        } else {
+          menu.lunch.push(row);
+        }
+      }
+    }
+
+    res.json(menu);
   } catch (error) {
-    res.status(500).send({ error: "failed to fetch the kafemud site" });
+    console.error("Error fetching or parsing the menu: ", error);
   }
 });
 
