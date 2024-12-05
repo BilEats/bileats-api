9e0c68c29428627508c31dc915df7b4c08e2c016
mrhonneynive <38505964+mrhonneynive@users.noreply.github.com>
2024-12-05 20:21:08 +0300

    Add menu endpoint

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Bileats API!');
 });
 
+app.get('/menu', (req, res) => {
+  res.json({ message: 'Menu endpoint is working!' });
+});
+
+
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+
