8e956aac21add4fa3d25f09dbc97fa628ce6ddc8
mrhonneynive <38505964+mrhonneynive@users.noreply.github.com>
2024-12-15 14:17:35 +0300

    Fix port binding for Heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const iconv = require("iconv-lite");
 const cheerio = require("cheerio");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Bileats API!");
